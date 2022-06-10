f02330a7efb7f5fc95dd1202555b7a9a8c6c9925
Usuario Visita <visitante@prof31.famaf.unc.edu.ar>
2022-06-10 18:29:52 -0300

    se agrego delay y sistema principal del sistema

--- app.s
+++ app.s
@@ -3,20 +3,16 @@
 .equ SCREEN_HEIGHT, 		480
 .equ BITS_PER_PIXEL,  	32
 
-
-// x1: y del punto inicial
-// x2: x del punto inicial  
+// x1: x del punto inicial
+// x2: y del punto inicial  
 // x3: ancho
 // x4: alto
 // x10: color(usamos w10 porque el pixel ocupa 32 bits)
 rectangle:
-	
 	mov x11, 0 // contador de filas
 	rows_loop:		
-		sub x1, x1, x9
 		mov x9, 0 // contador de columna por fila, lo reiniciamos para cada fila pintada
-		row_loop:	
-			
+		row_loop:		
 			// Llamada a pixel dir
 			sub sp, sp, 64	
 			stur x30, [sp, 56] 
@@ -27,6 +23,8 @@ rectangle:
 			stur x9, [sp, 16]
 			stur x10, [sp, 8]
 			stur x11, [sp, 0]
+			add x1, x9, x1
+			add x2, x11, x2
 			bl utils.pixeldir
 			ldur x11, [sp, 0]
 			ldur x10, [sp, 8]
@@ -40,12 +38,9 @@ rectangle:
 
 			stur w10, [x0]// pintar el punto
 			add x9, x9, 1
-			add x1, x1, 1
 			cmp x9, x3
 			b.lt row_loop
 		add x11, x11, 1
-		add x2, x2, 1
-
 		cmp x11, x4
 		b.lt rows_loop
 	ret
@@ -68,14 +63,36 @@ main:
 	movz x10, 0x77, lsl 16
 	movk x10, 0xF585, lsl 00
 	
-	mov x1, 10
-	mov x2, 10
-	mov x3, 20
-	mov x4, 40
-	bl rectangle
+	mov x1, 0
+	mov x2, 0
+	mov x3, 50
+	mov x4, 50
 
-	//---------------------------------------------------------------
-	// Infinite Loop 
+	mov x6, 5
+
+main_loop:
+
+	bl utils.save_registers
+	movz x10, 0x77, lsl 16
+	movk x10, 0xF585, lsl 00
+	bl rectangle
+	mov x5,300
+	bl utils.delay 
+	bl utils.restore_registers
+	
+	bl utils.save_registers
+	mov x10, 0
+	mov x1, 0
+	mov x2, 0
+	mov x3, 640
+	mov x4, 480
+	bl rectangle
+	bl utils.restore_registers
+
+	add x0,x0,50	
+	subs x6,x6,1
+	bne main_loop
+
 
 InfLoop: 
 	b InfLoop
--- utils.s
+++ utils.s
@@ -11,11 +11,62 @@
 //    x0 = x20 + 4(x1 + SCREEN_WIDTH * x2)
 .globl utils.pixeldir
 utils.pixeldir:
-	mov x10, 4
 	mov x9, SCREEN_WIDTH
 	mul x2, x2, x9 
 	add x0, x1, x2
-	mul x0, x0, x10
+	lsl x0, x0, 2
 	add x0, x20, x0
 	ret
 
+
+//args:
+	//x5: microseconds
+.globl utils.delay
+utils.delay:
+	sub sp,sp,8	//me encargo de dejar x5 como  estaba antes
+	stur x5,[sp,0]
+	lsl x5,x5,19
+	subs x5,x5,xzr
+	b.le delay_ret
+delay_loop:
+	add xzr, xzr, xzr //nop
+	subs x5,x5,1
+	b.ne delay_loop 
+delay_ret:
+
+	ldur x5,[sp,0]
+	add sp,sp,8	
+	ret
+
+
+//guarda los registros del 0 al 9
+.global utils.save_registers
+utils.save_registers:
+	sub sp,sp,80	
+	stur x0,[sp,0]
+	stur x1,[sp,8]
+	stur x2,[sp,16]
+	stur x3,[sp,24]
+	stur x4,[sp,32]
+	stur x5,[sp,40]
+	stur x6,[sp,48]
+	stur x7,[sp,56]
+	stur x8,[sp,64]
+	stur x9,[sp,72]
+	ret
+
+//actualiza los registros con los valores en el stack
+.global utils.restore_registers
+utils.restore_registers:
+	ldur x0,[sp,0]
+	ldur x1,[sp,8]
+	ldur x2,[sp,16]
+	ldur x3,[sp,24]
+	ldur x4,[sp,32]
+	ldur x5,[sp,40]
+	ldur x6,[sp,48]
+	ldur x7,[sp,56]
+	ldur x8,[sp,64]
+	ldur x9,[sp,72]
+	add sp,sp,80
+	ret
